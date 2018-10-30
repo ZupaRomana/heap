7af3bf1122f909f7dd1c48305fe45e081d1894af
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 15:55:19 +0100

    Implement add method when no root

--- src/main/java/com/codecool/HeapMin.java
+++ src/main/java/com/codecool/HeapMin.java
@@ -10,12 +10,42 @@ public class HeapMin {
 
     public HeapMin() {
         this.root = null;
-        this.nodes = null;
         this.size = 0;
+        this.nodes = new Node[size];
     }
 
-    public void add(int i) {
-        setRoot(new Node(i, null));
+    public void add(int value) {
+        Node node = createNode(value);
+        addToNodes(node);
+        if (root == null) {
+            initializeRoot(node);
+        }
+    }
+
+    private void increaseSize() {
+        size++;
+    }
+
+    private void addToNodes(Node node) {
+        extendArray();
+        nodes[size - 1] = node;
+    }
+
+    private void extendArray() {
+        Node[] newArr = new Node[size + 1];
+        for (int i = 0; i < size; i++) {
+            newArr[i] = nodes[i];
+        }
+        nodes = newArr;
+        increaseSize();
+    }
+
+    private Node createNode(int value) {
+        return new Node(value);
+    }
+
+    private void initializeRoot(Node root) {
+        setRoot(root);
     }
 
     private Node getRoot() throws NoSuchElementException {
