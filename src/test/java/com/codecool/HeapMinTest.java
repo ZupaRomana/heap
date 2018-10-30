149626da6578ad8fa5ca942c8427c108451b0e76
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 15:26:43 +0100

    Add field for testing case, add test checking getRoot when empty heap

--- src/test/java/com/codecool/HeapMinTest.java
+++ src/test/java/com/codecool/HeapMinTest.java
@@ -1,7 +1,20 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeapMinTest {
+
+    private HeapMin heap = new HeapMin();
+
+    @Test
+    void throwsException_when_noElements_getRootInvoked() {
+        assertThrows(NoSuchElementException.class, () -> {
+            heap.getRoot();
+        });
+    }
 
 }
