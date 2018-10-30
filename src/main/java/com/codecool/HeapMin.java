506330c0d3e3872ae395ddf5f840468a6550f651
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 10:54:59 +0100

    Change classes names to -Min

--- src/main/java/com/codecool/HeapMin.java
+++ src/main/java/com/codecool/HeapMin.java
@@ -1,4 +1,4 @@
 package com.codecool;
 
-public class Heap {
+public class HeapMin {
 }
--- src/test/java/com/codecool/HeapMinTest.java
+++ src/test/java/com/codecool/HeapMinTest.java
@@ -2,6 +2,6 @@ package com.codecool;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class HeapTest {
+class HeapMinTest {
 
 }
