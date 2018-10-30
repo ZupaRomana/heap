00b550e544cc182a2ad0682ca497527af77998d1
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 13:25:38 +0100

    Create test class; add test checking getSiblingNodes method

---
+++ src/test/java/com/codecool/NodeTest.java
@@ -0,0 +1,23 @@
+package com.codecool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @Test
+    void returnsValidNodesWithValues_getSiblingNodesInvoked() {
+        Node node = new Node(5, null);
+        Node left = new Node(3, node);
+        Node right = new Node(1, node);
+        node.setLeft(left);
+        node.setRight(right);
+
+        Node[] expected = {left, right};
+        Node[] actual = node.getSiblingNodes();
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
