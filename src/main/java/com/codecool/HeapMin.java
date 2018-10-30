bf7d3603f172adebdd9f75894d8909827c009875
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 14:58:28 +0100

    Implement fields, constructor, accessors, modifiers

--- src/main/java/com/codecool/HeapMin.java
+++ src/main/java/com/codecool/HeapMin.java
@@ -1,4 +1,26 @@
 package com.codecool;
 
 public class HeapMin {
+
+    private Node root;
+    private Node[] nodes;
+
+    public HeapMin() {
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node[] getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Node[] nodes) {
+        this.nodes = nodes;
+    }
 }
