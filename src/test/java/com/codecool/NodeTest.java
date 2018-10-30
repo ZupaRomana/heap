807fbcf83ebcaaf4fdee121639b1696b46c8c2d4
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 13:47:34 +0100

    Set common used node as private field

--- src/test/java/com/codecool/NodeTest.java
+++ src/test/java/com/codecool/NodeTest.java
@@ -6,9 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NodeTest {
 
+    private Node node = new Node(5, null);
+
     @Test
     void returnsValidNodesWithValues_getSiblingNodesInvoked() {
-        Node node = new Node(5, null);
         Node left = new Node(3, node);
         Node right = new Node(1, node);
         node.setLeft(left);
@@ -22,14 +23,11 @@ class NodeTest {
 
     @Test
     void returnsTrue_when_nodeHasNoChildren() {
-        Node node = new Node(3, null);
-
         assertTrue(node.isLeaf());
     }
 
     @Test
     void returnsFalse_when_nodeHasOneChild() {
-        Node node = new Node(3, null);
         Node child = new Node(5, node);
 
         assertFalse(node.isLeaf());
