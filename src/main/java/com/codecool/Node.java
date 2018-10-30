a5963e74b7834b3767c034fa6e0d9f53ef1bb10c
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-30 13:14:52 +0100

    Add constructors, accessors, modifiers

--- src/main/java/com/codecool/Node.java
+++ src/main/java/com/codecool/Node.java
@@ -6,4 +6,41 @@ public class Node {
     private Node parent;
     private Node left;
     private Node right;
+
+    public Node(int value, Node parent) {
+        this.value = value;
+        this.parent = parent;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
